73c3b3ceaccd2c79e2efbe9217df58a4ba79a2af
Michael Gira <contact@michaelgira.me>
2016-08-24 12:23:10 -0500

    This form knows no borders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const port = 420;
 
+const _ = require('underscore');
+const http = require('http');
+
 const express = require('express');
 const app = express();
+const server = http.Server(app);
+const io = require('socket.io')(server);
+
+const levelTypes = [
+	'beginner',
+	'intermediate',
+	'advanced'
+];
 
 app.use('/', express.static(__dirname + '/public'));
 
@@ -9,6 +20,23 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/html/index.html');
 });
 
-app.listen(port, function() {
+server.listen(port, function() {
 	console.log('Server listening on *:' + port);
+});
+
+io.on('connection', function(socket) {
+	console.log('User connected!')
+
+	socket.on('signup', function(data) {
+		if(typeof data.email !== 'string' || data.email === '') {
+			socket.emit('signup response', false, 'Invalid email!');
+			return;
+		}
+		if(!_.contains(levelTypes, data.level)) {
+			socket.emit('signup response', false, 'Invalid programming level!');
+			return;
+		}
+
+		console.log('User signup!', data)
+	});
 });
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,7 +1,16 @@
+var socket = io();
 var $form = $('form');
 
 $form.submit(function(event) {
 	event.preventDefault();
-	var data = $(this).serialize();
-	console.log(data);
+
+	var data = $(this).serializeArray();
+	var formData = {};
+	for(var i = 0; i < data.length; i++) {
+		var formInput = data[i];
+		formData[formInput.name] = formInput.value;
+	}
+
+	socket.emit('signup', formData);
+	console.log(formData);
 });
